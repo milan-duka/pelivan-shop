3e234fbfd5e318455a1cde66750a7fb8714d2c8e
duka.milan <duka.milan@gmail.com>
2017-05-27 20:49:53 +0000

    Initially adding of project in repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_order
+
+  def current_order
+    if !session[:order_id].nil?
+      if Order.exists?(session[:order_id])
+        Order.find(session[:order_id])
+      else
+        Order.new
+      end
+    else
+      Order.new
+    end
+  end
+end
